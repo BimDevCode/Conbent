8d800ca1059fa948c5e43501476ef36c5f9fdc21
Mikalai <nikolaysobolewskij@gmail.com>
2024-02-16 11:32:57 +0100

    feat: add main page

---
+++ src/WebApp/WebClient/src/app/feature/main-page/components/ancient-greek-part/ancient-greek-part.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ancient-greek-part',
+  templateUrl: './ancient-greek-part.component.html',
+  styleUrl: './ancient-greek-part.component.scss'
+})
+export class AncientGreekPartComponent {
+  imageUrlA = '../../../../../assets/Image/OrderPartA.jpeg';
+  imageUrlB = '../../../../../assets/Image/OrderPartB.jpeg';
+  imageUrlC = '../../../../../assets/Image/OrderPartC.jpeg';
+  imageUrlD = '../../../../../assets/Image/OrderPartD.jpeg';
+  imageUrlE = '../../../../../assets/Image/OrderPartE.jpeg';
+  imageUrlF = '../../../../../assets/Image/OrderPartF.jpeg';
+  imageUrlG = '../../../../../assets/Image/OrderPartG.jpeg';
+}
--- src/WebApp/WebClient/src/app/feature/main-page/main-page.module.ts
+++ src/WebApp/WebClient/src/app/feature/main-page/main-page.module.ts
@@ -2,19 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { IntroComponent } from './components/intro/intro.component';
+import { AncientGreekPartComponent } from './components/ancient-greek-part/ancient-greek-part.component';
+import { NextStepPartComponent } from './components/next-step-part/next-step-part.component';
+import { ParallaxADirective } from './components/parallax-a.directive';
+
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    IntroComponent,
+    AncientGreekPartComponent,
+    NextStepPartComponent,
+    ParallaxADirective
+
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    MainPageComponent
+    MainPageComponent,
+    IntroComponent,
+    AncientGreekPartComponent,
+    NextStepPartComponent,
+    ParallaxADirective
+
   ]
 })
 export class MainPageModule { }
