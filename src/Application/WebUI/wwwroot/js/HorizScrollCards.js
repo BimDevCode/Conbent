4193447b760e11ddef6028f28a90742e3ea53891
MikalaiSab <mikalai.sabaleuski@axiomint.com>
2024-02-22 12:52:36 +0100

    🔨fix: remove old Web UI project

--- src/Application/WebUI/wwwroot/js/HorizScrollCards.js
+++
@@ -1,9 +0,0 @@
-﻿$('.main-carousel').flickity({
-    cellAlign: 'center',
-    contain: true,
-    dragThreshold: 10,
-    freeScroll: true,
-    wrapAround: false,
-    groupCells: true,
-    prevNextButtons: false
-});
