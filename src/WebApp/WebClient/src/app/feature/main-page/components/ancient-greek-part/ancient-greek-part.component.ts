b77a34cc58332b3afd741037d38be24572d73ef1
MikalaiSab <mikalai.sabaleuski@axiomint.com>
2024-03-11 15:02:54 +0100

    feat: add card courusel

--- src/WebApp/WebClient/src/app/feature/main-page/components/ancient-greek-part/ancient-greek-part.component.ts
+++ src/WebApp/WebClient/src/app/feature/main-page/components/ancient-greek-part/ancient-greek-part.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { gsap } from "gsap";
+import $ from 'jquery';//Leave for JS sript
 
 @Component({
   selector: 'app-ancient-greek-part',
   templateUrl: './ancient-greek-part.component.html',
   styleUrl: './ancient-greek-part.component.scss'
 })
-export class AncientGreekPartComponent implements OnInit {
+export class AncientGreekPartComponent implements AfterViewInit {
 [x: string]: any;
   imageUrlA = '../../../../../assets/Image/OrderPartA.jpeg';
   imageUrlB = '../../../../../assets/Image/OrderPartB.jpeg';
@@ -15,7 +20,21 @@ export class AncientGreekPartComponent implements OnInit {
   imageUrlF = '../../../../../assets/Image/OrderPartF.jpeg';
   imageUrlG = '../../../../../assets/Image/OrderPartG.jpeg';
 
-  
-  ngOnInit() {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {
   }
+  ngAfterViewInit(): void {
+    if(isPlatformBrowser(this.platformId)) {
+      this.loadScript('https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js');
+      this.loadScript('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.3/gsap.min.js');
+      //this.loadScript('../../../../../assets/js/main-page/carousel.js');
+    }
+  }
+
+  loadScript(urnName: string){
+    const s = this.renderer.createElement('script');
+    s.type = 'text/javascript';
+    s.src = urnName; // replace with the actual path to your script
+    this.renderer.appendChild(this.document.body, s);
+  }
+
 }
